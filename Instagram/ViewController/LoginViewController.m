0741971c12005767aa23b5a56249122eb236d4bc
Jacqueline Jou <jacquelinejou@fb.com>
2022-06-27 16:13:29 -0700

    Added functionality for login and signup. Added a helper method to reduce redundancy for if user tries to submit with empty field. On success, brings to home feed view.

--- Instagram/ViewController/LoginViewController.m
+++ Instagram/ViewController/LoginViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -15,6 +18,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+- (IBAction)loginUser:(id)sender {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
+        [self loginHelper];
+    } else {
+       [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+           if (error == nil) {
+               [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+           }
+       }];
+    }
+}
+
+- (IBAction)registerUser:(id)sender {
+    // Create user
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
+        [self loginHelper];
+    } else {
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+           if (error == nil) {
+               [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+           }
+       }];
+    }
+}
+
+-(void)loginHelper {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Empty Field" message:@"Please fill in this field." preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    // create a cancel action
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        self.usernameField.text = @"Username";
+        self.passwordField.text = @"Password";
+    }];
+    // add the cancel action to the alertController
+    [alert addAction:cancelAction];
+
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    [self presentViewController:alert animated:YES completion:^{
+    }];
 }
 
 @end
