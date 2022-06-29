83f93632a49b6f6f10028f7a06b9b834731a9882
Jacqueline Jou <jacquelinejou@fb.com>
2022-06-29 10:30:08 -0700

    Added a post detail page where users can click on a post and see it in a different page with the post image, caption, date, user, and number of likes.

---
+++ Instagram/ViewController/PostDetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  PostDetailsViewController.h
+//  Instagram
+//
+//  Created by jacquelinejou on 6/29/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "Post.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostDetailsViewController : UIViewController
+
+@property (nonatomic, strong) Post *detailPost;
+
+@end
+
+NS_ASSUME_NONNULL_END
