3d9ebb0c0e9ed0e211412ab6bc4bfb3ea42d0487
Jacqueline Jou <jacquelinejou@fb.com>
2022-06-29 16:56:52 -0700

    Added section headers on timeline to show username and time stamp.

--- Instagram/ViewController/HomeFeedViewController.m
+++ Instagram/ViewController/HomeFeedViewController.m
@@ -15,7 +15,6 @@
 #import "ComposeViewController.h"
 #import "InfiniteScrollViewController.h"
 #import "PostDetailsViewController.h"
-#import "TableCell.h"
 
 @interface HomeFeedViewController () <UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate, PostDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -25,18 +24,12 @@
 @end
 
 @implementation HomeFeedViewController
-NSArray *data;
-NSString *CellIdentifier = @"TableViewCell";
-NSString *HeaderViewIdentifier = @"TableViewHeaderView";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     self.arrayOfPosts = [[NSMutableArray alloc] init];
-
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
-    [self.tableView registerClass:[UITableViewHeaderFooterView class] forHeaderFooterViewReuseIdentifier:HeaderViewIdentifier];
     
     self.navigationController.navigationBar.topItem.title = @"Home Feed";
     UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
@@ -148,17 +141,35 @@ NSString *HeaderViewIdentifier = @"TableViewHeaderView";
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    TableCell *header = [tableView dequeueReusableHeaderFooterViewWithIdentifier:@"TableCell"];
-    Post *post = self.arrayOfPosts[section];
-    header.usernameLabel.text = post.author.username;
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, tableView.frame.size.height / 10)];
+    /* Create custom view to display section header... */
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 5, tableView.frame.size.width, tableView.frame.size.height / 10)];
+    [label setFont:[UIFont boldSystemFontOfSize:30]];
+    Post *post = [self.arrayOfPosts objectAtIndex:section];
+    NSString *string = post.author.username;
+    [label setText:string];
+    [view addSubview:label];
+    [view setBackgroundColor:[UIColor whiteColor]];
+    return view;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 18)];
+    /* Create custom view to display section header... */
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 5, tableView.frame.size.width, 18)];
+    [label setFont:[UIFont boldSystemFontOfSize:20]];
+    Post *post = [self.arrayOfPosts objectAtIndex:section];
     NSDate *postTime = post.createdAt;
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
     formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
     // Configure the input format to parse the date string
     formatter.dateStyle = NSDateFormatterShortStyle;
     formatter.timeStyle = NSDateFormatterShortStyle;
-    header.timeLabel.text = [formatter stringFromDate:postTime];
-    return header;
+    NSString *string = [formatter stringFromDate:postTime];
+    [label setText:string];
+    [view addSubview:label];
+    [view setBackgroundColor:[UIColor whiteColor]];
+    return view;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
